e6c79562b3bcfe45c732bb09d1a6d6dd4b32822f
eliasrilegard <e_rilegard@live.se>
2025-06-21 17:02:07 +0200

    Consolidate colorschemes and management

--- .config/nvim/lua/rilegard/plugins/colorscheme.lua
+++ .config/nvim/lua/rilegard/plugins/colorscheme.lua
@@ -1,17 +1,53 @@
 return {
-  {
-    "AlexvZyl/nordic.nvim",
-    priority = 1000,
-    opts = {
-      on_highlight = function(highlights, _palette)
-        for _, highlight in pairs(highlights) do
-          highlight.italic = false
-        end
-      end,
+  "zaldih/themery.nvim",
+  cmd = "Themery",
+  event = "VimEnter",
+  keys = {
+    { "<leader>T", "<CMD>Themery<CR>", desc = "Switch Theme" },
+  },
+  opts = {
+    themes = {
+      {
+        name = "Nordic",
+        colorscheme = "nordic",
+      },
+      {
+        name = "Kanagawa",
+        colorscheme = "kanagawa-wave",
+      },
     },
+    livePreview = true,
   },
-  {
-    "tomasiser/vim-code-dark",
-    priority = 1000,
-  },
+  dependencies = {
+    {
+      "AlexvZyl/nordic.nvim",
+      opts = {
+        on_highlight = function(highlights, _palette)
+          for _, highlight in pairs(highlights) do
+            highlight.italic = false
+          end
+        end,
+      },
+    },
+    {
+      "rebelot/kanagawa.nvim",
+      opts = {
+        commentStyle = { italic = false },
+        keywordStyle = { italic = false },
+        statementStyle = { bold = false },
+        colors = {
+          theme = {
+            all = {
+              ui = { bg_gutter = "none" },
+            },
+          },
+        },
+        overrides = function()
+          return {
+            Boolean = { bold = false },
+          }
+        end,
+      },
+    },
+  }
 }
