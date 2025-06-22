42ceea00da1d170e16ef7dad19bc20bbd960f64c
eliasrilegard <e_rilegard@live.se>
2025-06-22 14:47:33 +0200

    Use declarative config

--- .config/nvim/lua/rilegard/plugins/treesitter.lua
+++ .config/nvim/lua/rilegard/plugins/treesitter.lua
@@ -2,16 +2,15 @@ return {
   "nvim-treesitter/nvim-treesitter",
   event = "BufReadPost",
   build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      highlight = { enable = true },
-      indent = { enable = true },
-      ensure_installed = {
-        "lua",
-        "python",
-        "rust",
-      },
-      sync_install = false,
-    })
-  end,
+  main = "nvim-treesitter.configs",
+  opts = {
+    highlight = { enable = true },
+    indent = { enable = true },
+    ensure_installed = {
+      "lua",
+      "python",
+      "rust",
+    },
+    sync_install = false,
+  },
 }
