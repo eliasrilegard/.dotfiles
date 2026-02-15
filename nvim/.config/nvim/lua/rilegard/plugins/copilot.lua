ca5131ad52f1e03beb64b02d57347f72cb9b47d1
eliasrilegard <e_rilegard@live.se>
2026-02-16 00:46:22 +0100

    Move to copilot.lua and add lualine integration

--- nvim/.config/nvim/lua/rilegard/plugins/copilot.lua
+++ nvim/.config/nvim/lua/rilegard/plugins/copilot.lua
@@ -1,4 +1,35 @@
 return {
-  "github/copilot.vim",
+  "zbirenbaum/copilot.lua",
   event = "BufReadPost",
+  opts = {
+    suggestion = {
+      auto_trigger = true,
+      keymap = {
+        accept = "<C-l>",
+        accept_word = false,
+        accept_line = false,
+        next = "<C-j>",
+        prev = "<C-k>",
+        dismiss = "<C-h>",
+      },
+    },
+  },
+  config = function(_, opts)
+    -- Hide Copilot suggestions when completion menu is open
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "BlinkCmpMenuOpen",
+      callback = function()
+        vim.b.copilot_suggestion_hidden = true
+      end,
+    })
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "BlinkCmpMenuClose",
+      callback = function()
+        vim.b.copilot_suggestion_hidden = false
+      end,
+    })
+
+    vim.keymap.set({ "n", "i" }, "<C-CR>", "<cmd>Copilot toggle<cr>", { desc = "Toggle Copilot" })
+    require("copilot").setup(opts)
+  end,
 }
--- nvim/.config/nvim/lua/rilegard/plugins/lualine.lua
+++ nvim/.config/nvim/lua/rilegard/plugins/lualine.lua
@@ -1,7 +1,10 @@
 return {
   "nvim-lualine/lualine.nvim",
   event = "VimEnter",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+    "AndreM222/copilot-lualine",
+  },
   config = function()
     local lualine = require("lualine")
     local lazy_status = require("lazy.status")
@@ -25,6 +28,22 @@ return {
             lazy_status.updates,
             cond = lazy_status.has_updates,
             color = { fg = "#ff9e64" },
+          },
+          {
+            "copilot",
+            show_colors = true,
+            show_loading = false,
+            symbols = {
+              status = {
+                icons = {
+                  warning = "",
+                  unknown = "",
+                },
+                hl = {
+                  enabled = "#98bb6c",
+                }
+              },
+            },
           },
           "filetype",
         },
